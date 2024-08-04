1c4792970e91c98c915420e1451d639289d5a4f8
ianramy <imwagore@gmail.com>
2024-08-04 18:04:49 +0300

    origin/master

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   return (
+    <div id="navbar">
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
+    </div>
   );
 }
+
+export default NavBar;
